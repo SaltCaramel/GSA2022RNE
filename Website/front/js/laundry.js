eb30c9f2cb11bc6a3e322e47c5eaa43cea2d5f93
parkbs6 <penel1374@gmail.com>
2022-11-29 17:30:02 +0900

    oh

--- Website/front/js/laundry.js
+++ Website/front/js/laundry.js
@@ -1,7 +1,12 @@
-function laundry(){
-    fetch("http://10.59.14.61:5000/laundrycheck")
+  function laundry(){
+    fetch("http://10.82.119.182:5000/laundrycheck")
     .then((response) => {
-       // console.log(`response!!! =  ${response}`)
-        return response
+        console.log(`response!!! =  ${response}`)
+        return response.json()
+    })
+    .then((data) => {
+        console.log(`data = ${data}`)
+        laun = res = String(data['laundry']);;
+        document.getElementById("laun").value = laun;
     })
 }  
